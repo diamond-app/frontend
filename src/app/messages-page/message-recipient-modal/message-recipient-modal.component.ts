d426d9e6cca0127e9330b5ed656d0b93ab4f8baa
superzordon <zordonclout@gmail.com>
2021-10-07 21:01:13 -0700

    Small formatting tweaks

--- src/app/messages-page/message-recipient-modal/message-recipient-modal.component.ts
+++ src/app/messages-page/message-recipient-modal/message-recipient-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 import { GlobalVarsService } from "../../global-vars.service";
 import { BsModalRef } from "ngx-bootstrap/modal";
 
@@ -7,8 +7,9 @@ import { BsModalRef } from "ngx-bootstrap/modal";
   templateUrl: "./message-recipient-modal.component.html",
   styleUrls: ["./message-recipient-modal.component.scss"],
 })
-export class MessageRecipientModalComponent {
+export class MessageRecipientModalComponent implements AfterViewInit {
   @Output() userSelected = new EventEmitter();
+  @ViewChild("searchbar", { static: true }) searchBar: ElementRef;
   searchHasResults = false;
   constructor(public globalVars: GlobalVarsService, public bsModalRef: BsModalRef) {}
   _handleCreatorSelectedInSearch(event) {
@@ -17,5 +18,13 @@ export class MessageRecipientModalComponent {
   }
   _handleSearchUpdated(event) {
     this.searchHasResults = event;
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const searchElement = document.querySelector(".message-recipient-modal__header #searchbar");
+      // @ts-ignore
+      searchElement.focus();
+    }, 0);
   }
 }
