f5adfa7666a96b2e1372f420fab6f142a9b9e935
superzordon <zordonclout@gmail.com>
2021-10-12 20:57:04 -0700

    Fixes to amplitude logging, updates to referral link display

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -61,6 +61,8 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   pullToRefreshHandler;
 
+  referralExpiration = new Date("2021-10-19T22:00:00.000Z");
+
   // This is [Following, Global, Market] if the user is following anybody. Otherwise,
   // it's [Global, Following, Market].
   //
