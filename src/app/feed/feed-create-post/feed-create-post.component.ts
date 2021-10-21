16db96a54e856f5da0eea0a70d57cdc3801c662e
superzordon <zordonclout@gmail.com>
2021-10-20 23:51:21 -0400

    Add autofill to mentions

--- src/app/feed/feed-create-post/feed-create-post.component.ts
+++ src/app/feed/feed-create-post/feed-create-post.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, Input, EventEmitter, Output, ViewChild } from "@angular/core";
 import { GlobalVarsService } from "../../global-vars.service";
-import { BackendApiService, BackendRoutes, PostEntryResponse } from "../../backend-api.service";
+import { BackendApiService, BackendRoutes, PostEntryResponse, ProfileEntryResponse } from "../../backend-api.service";
 import { Router, ActivatedRoute } from "@angular/router";
 import { SharedDialogs } from "../../../lib/shared-dialogs";
 import { CdkTextareaAutosize } from "@angular/cdk/text-field";
@@ -9,6 +9,7 @@ import { environment } from "../../../environments/environment";
 import * as tus from "tus-js-client";
 import Timer = NodeJS.Timer;
 import { CloudflareStreamService } from "../../../lib/services/stream/cloudflare-stream-service";
+import * as _ from "lodash";
 
 @Component({
   selector: "feed-create-post",
@@ -49,7 +50,7 @@ export class FeedCreatePostComponent implements OnInit {
     "E.T. phone home",
     "Elementary, my dear Watson",
     "I'm going to make him an offer he can't refuse.",
-    "Big things have small beginnings."
+    "Big things have small beginnings.",
   ];
 
   submittingPost = false;
@@ -72,6 +73,7 @@ export class FeedCreatePostComponent implements OnInit {
   globalVars: GlobalVarsService;
   GlobalVarsService = GlobalVarsService;
 
+  users = [{ username: "john_doe" }, { username: "jane_doe" }];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -83,12 +85,40 @@ export class FeedCreatePostComponent implements OnInit {
     this.globalVars = appData;
   }
 
+  resolveFn = (prefix) => this.getUsersFromPrefix(prefix);
+
+  async getUsersFromPrefix(prefix) {
+    const profiles = await this.backendApi
+      .GetProfiles(
+        this.globalVars.localNode,
+        "" /*PublicKeyBase58Check*/,
+        "" /*Username*/,
+        prefix.trim().replace(/^@/, "") /*UsernamePrefix*/,
+        "" /*Description*/,
+        "" /*Order by*/,
+        5 /*NumToFetch*/,
+        this.globalVars.loggedInUser.PublicKeyBase58Check /*ReaderPublicKeyBase58Check*/,
+        "" /*ModerationType*/,
+        false /*FetchUsersThatHODL*/,
+        false /*AddGlobalFeedBool*/
+      )
+      .toPromise();
+    return profiles.ProfilesFound;
+  }
+
   ngOnInit() {
     this.isComment = !this.isQuote && !!this.parentPost;
     this._setRandomMovieQuote();
     if (this.inTutorial) {
       this.postInput = "It's Diamond time!";
     }
+    new Mentionify(
+      document.getElementById("textarea"),
+      document.getElementById("menu"),
+      this.resolveFn,
+      replaceFn,
+      menuItemFn
+    );
   }
 
   onPaste(event: any): void {
@@ -361,3 +391,244 @@ export class FeedCreatePostComponent implements OnInit {
     }, timeoutMillis);
   }
 }
+
+const properties = [
+  "direction",
+  "boxSizing",
+  "width",
+  "height",
+  "overflowX",
+  "overflowY",
+
+  "borderTopWidth",
+  "borderRightWidth",
+  "borderBottomWidth",
+  "borderLeftWidth",
+  "borderStyle",
+
+  "paddingTop",
+  "paddingRight",
+  "paddingBottom",
+  "paddingLeft",
+
+  "fontStyle",
+  "fontVariant",
+  "fontWeight",
+  "fontStretch",
+  "fontSize",
+  "fontSizeAdjust",
+  "lineHeight",
+  "fontFamily",
+
+  "textAlign",
+  "textTransform",
+  "textIndent",
+  "textDecoration",
+
+  "letterSpacing",
+  "wordSpacing",
+
+  "tabSize",
+  "MozTabSize",
+];
+
+const isFirefox = typeof window !== "undefined" && window["mozInnerScreenX"] != null;
+
+/**
+ * @param {HTMLTextAreaElement} element
+ * @param {number} position
+ */
+function getCaretCoordinates(element, position) {
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+
+  const style = div.style;
+  const computed = getComputedStyle(element);
+
+  style.whiteSpace = "pre-wrap";
+  style.wordWrap = "break-word";
+  style.position = "absolute";
+  style.visibility = "hidden";
+
+  properties.forEach((prop) => {
+    style[prop] = computed[prop];
+  });
+
+  if (isFirefox) {
+    if (element.scrollHeight > parseInt(computed.height)) style.overflowY = "scroll";
+  } else {
+    style.overflow = "hidden";
+  }
+
+  div.textContent = element.value.substring(0, position);
+
+  const span = document.createElement("span");
+  span.textContent = element.value.substring(position) || ".";
+  div.appendChild(span);
+
+  const coordinates = {
+    top: span.offsetTop + parseInt(computed["borderTopWidth"]),
+    left: span.offsetLeft + parseInt(computed["borderLeftWidth"]),
+    // height: parseInt(computed['lineHeight'])
+    height: span.offsetHeight,
+  };
+
+  div.remove();
+
+  return coordinates;
+}
+
+class Mentionify {
+  private ref: any;
+  private menuRef: any;
+  private resolveFn: any;
+  private replaceFn: any;
+  private menuItemFn: any;
+  private options: any;
+  private left: any;
+  private top: any;
+  private triggerIdx: any;
+  private active: number;
+
+  constructor(ref, menuRef, resolveFn, replaceFn, menuItemFn) {
+    this.ref = ref;
+    this.menuRef = menuRef;
+    this.resolveFn = resolveFn;
+    this.replaceFn = replaceFn;
+    this.menuItemFn = menuItemFn;
+    this.options = [];
+
+    this.makeOptions = this.makeOptions.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+    this.selectItem = this.selectItem.bind(this);
+    this.onInput = this.onInput.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.renderMenu = this.renderMenu.bind(this);
+
+    this.ref.addEventListener("input", this.onInput);
+    this.ref.addEventListener("keydown", this.onKeyDown);
+  }
+
+  async makeOptions(query) {
+    const options = await this.resolveFn(query);
+    if (options.lenght !== 0) {
+      this.options = options;
+      this.renderMenu();
+    } else {
+      this.closeMenu();
+    }
+  }
+
+  closeMenu() {
+    setTimeout(() => {
+      this.options = [];
+      this.left = undefined;
+      this.top = undefined;
+      this.triggerIdx = undefined;
+      this.renderMenu();
+    }, 0);
+  }
+
+  selectItem(active) {
+    return () => {
+      const preMention = this.ref.value.substr(0, this.triggerIdx);
+      const option = this.options[active];
+      const mention = this.replaceFn(option, this.ref.value[this.triggerIdx]);
+      const postMention = this.ref.value.substr(this.ref.selectionStart);
+      const newValue = `${preMention}${mention}${postMention}`;
+      this.ref.value = newValue;
+      const caretPosition = this.ref.value.length - postMention.length;
+      this.ref.setSelectionRange(caretPosition, caretPosition);
+      this.closeMenu();
+      this.ref.focus();
+    };
+  }
+
+  onInput(ev) {
+    const positionIndex = this.ref.selectionStart;
+    const textBeforeCaret = this.ref.value.slice(0, positionIndex);
+    const tokens = textBeforeCaret.split(/\s/);
+    const lastToken = tokens[tokens.length - 1];
+    const triggerIdx = textBeforeCaret.endsWith(lastToken) ? textBeforeCaret.length - lastToken.length : -1;
+    const maybeTrigger = textBeforeCaret[triggerIdx];
+    const keystrokeTriggered = maybeTrigger === "@";
+
+    if (!keystrokeTriggered) {
+      this.closeMenu();
+      return;
+    }
+
+    const query = textBeforeCaret.slice(triggerIdx + 1);
+    this.makeOptions(query);
+
+    const coords = getCaretCoordinates(this.ref, positionIndex);
+    const { top, left } = this.ref.getBoundingClientRect();
+
+    setTimeout(() => {
+      this.active = 0;
+      this.left = window.scrollX + coords.left + left + this.ref.scrollLeft;
+      this.top = window.scrollY + coords.top + top + coords.height - this.ref.scrollTop;
+      this.triggerIdx = triggerIdx;
+      this.renderMenu();
+    }, 0);
+  }
+
+  onKeyDown(ev) {
+    let keyCaught = false;
+    if (this.triggerIdx !== undefined) {
+      switch (ev.key) {
+        case "ArrowDown":
+          this.active = Math.min(this.active + 1, this.options.length - 1);
+          this.renderMenu();
+          keyCaught = true;
+          break;
+        case "ArrowUp":
+          this.active = Math.max(this.active - 1, 0);
+          this.renderMenu();
+          keyCaught = true;
+          break;
+        case "Enter":
+        case "Tab":
+          this.selectItem(this.active)();
+          keyCaught = true;
+          break;
+      }
+    }
+
+    if (keyCaught) {
+      ev.preventDefault();
+    }
+  }
+
+  renderMenu() {
+    if (this.top === undefined) {
+      this.menuRef.hidden = true;
+      return;
+    }
+
+    this.menuRef.style.left = this.left + "px";
+    this.menuRef.style.top = this.top + "px";
+    this.menuRef.innerHTML = "";
+
+    this.options.forEach((option, idx) => {
+      this.menuRef.appendChild(this.menuItemFn(option, this.selectItem(idx), this.active === idx));
+    });
+
+    this.menuRef.hidden = false;
+  }
+}
+
+const replaceFn = (user, trigger) => `${trigger}${user.Username} `;
+
+const menuItemFn = (user, setItem, selected) => {
+  const div = document.createElement("div");
+  div.setAttribute("role", "option");
+  div.className = "menu-item";
+  if (selected) {
+    div.classList.add("selected");
+    div.setAttribute("aria-selected", "");
+  }
+  div.textContent = user.Username;
+  div.onclick = setItem;
+  return div;
+};
