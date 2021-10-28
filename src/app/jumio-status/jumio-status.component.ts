39a53eb4a57e145e99e2fef9a3d1f9aaf2153651
superzordon <88362450+superzordon@users.noreply.github.com>
2021-10-28 15:23:19 -0400

    Add prompt to let user hide free money banner, add padding to referral banner (#52)

--- src/app/jumio-status/jumio-status.component.ts
+++ src/app/jumio-status/jumio-status.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from "@angular/core";
 import { GlobalVarsService } from "../global-vars.service";
+import { SwalHelper } from "../../lib/helpers/swal-helper";
 
 @Component({
   selector: "jumio-status",
@@ -9,4 +10,27 @@ import { GlobalVarsService } from "../global-vars.service";
 export class JumioStatusComponent {
   @Input() message: string = "Click here to get";
   constructor(public globalVars: GlobalVarsService) {}
+
+  hideFreeMoneyBanner(event) {
+    event.stopPropagation();
+    SwalHelper.fire({
+      target: this.globalVars.getTargetComponentSelector(),
+      title: "Hide banner",
+      html: "Do you want to hide the free money banner?",
+      showCancelButton: true,
+      showConfirmButton: true,
+      focusConfirm: true,
+      customClass: {
+        confirmButton: "btn btn-light",
+        cancelButton: "btn btn-light no",
+      },
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+      reverseButtons: true,
+    }).then(async (alertRes: any) => {
+      if (alertRes.isConfirmed) {
+        this.globalVars.setShowFreeMoneyBanner(false);
+      }
+    });
+  }
 }
