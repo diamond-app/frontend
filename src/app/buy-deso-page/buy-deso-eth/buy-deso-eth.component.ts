cda6c1c7f24cd58b7cd3dc62790561b5978fb834
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-11-20 00:32:22 -0500

    fix ETH calculations when desoToBuy changes (#81)

--- src/app/buy-deso-page/buy-deso-eth/buy-deso-eth.component.ts
+++ src/app/buy-deso-page/buy-deso-eth/buy-deso-eth.component.ts
@@ -433,7 +433,7 @@ export class BuyDeSoEthComponent implements OnInit {
     // Make sure that value + actual fees does not exceed the current balance. If it does, subtract the remainder from value.
     let value = this.weiToExchange.sub(totalFees);
     let remainder = totalFees.add(value).sub(this.weiBalance);
-    if (remainder.gt(0)) {
+    if (remainder.gt(new BN(0))) {
       value = value.sub(remainder);
     }
     return toHex(value);
@@ -472,7 +472,7 @@ export class BuyDeSoEthComponent implements OnInit {
   }
 
   computeWeiToBurnGivenDESONanos(amountNanos: number): BN {
-    const weiMinusFees = new BN(amountNanos).div(this.getExchangeRateAfterFee());
+    const weiMinusFees = new BN(amountNanos).mul(this.getWeiPerNanoExchangeRate());
     return weiMinusFees.add(this.weiFeeEstimate);
   }
 
@@ -481,11 +481,16 @@ export class BuyDeSoEthComponent implements OnInit {
     if (weiMinusFees.ltn(0)) {
       return new BN(0);
     }
-    return Number(fromWei(weiMinusFees)) * this.getExchangeRateAfterFee().toNumber();
+    return weiMinusFees.div(this.getWeiPerNanoExchangeRate()).toNumber();
   }
 
+  // Note this returns nanos per ETH.
   getExchangeRateAfterFee(): BN {
     return new BN(this.globalVars.nanosPerETHExchangeRate).mul(new BN(this.nodeFee()));
+  }
+
+  getWeiPerNanoExchangeRate(): BN {
+    return toWei(new BN(1)).div(this.getExchangeRateAfterFee());
   }
 
   updateDESOToBuy(newVal) {
