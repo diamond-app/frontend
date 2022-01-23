33586e9c44e1ed3d604e29fc78e3fb2deffd7726
superzordon <88362450+superzordon@users.noreply.github.com>
2022-01-23 17:30:48 -0500

    Add copy profile to dropdown, fix copy on jumio banner (#143)

--- src/app/jumio-status/jumio-status.component.ts
+++ src/app/jumio-status/jumio-status.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from "@angular/core";
 import { GlobalVarsService } from "../global-vars.service";
 import { SwalHelper } from "../../lib/helpers/swal-helper";
+import { TranslocoService } from "@ngneat/transloco";
 
 @Component({
   selector: "jumio-status",
@@ -9,14 +10,14 @@ import { SwalHelper } from "../../lib/helpers/swal-helper";
 })
 export class JumioStatusComponent {
   @Input() message: string = "jumio_status.click_here_to_get";
-  constructor(public globalVars: GlobalVarsService) {}
+  constructor(public globalVars: GlobalVarsService, private translocoService: TranslocoService) {}
 
   hideFreeMoneyBanner(event) {
     event.stopPropagation();
     SwalHelper.fire({
       target: this.globalVars.getTargetComponentSelector(),
-      title: "jumio_status.hide_banner",
-      html: "jumio_status.hide_banner_message",
+      title: this.translocoService.translate("jumio_status.hide_banner"),
+      html: this.translocoService.translate("jumio_status.hide_banner_message"),
       showCancelButton: true,
       showConfirmButton: true,
       focusConfirm: true,
