75a24303b3cb9437963a6acaf5806cb4249d63f5
superzordon <zordonclout@gmail.com>
2022-08-02 18:13:16 -0400

    Fix loading screen for hashtags, fix hashtags when logged out, changed default right bar to hashtags, added border to nft pfp

--- src/app/right-bar-creators/right-bar-creators.component.ts
+++ src/app/right-bar-creators/right-bar-creators.component.ts
@@ -74,7 +74,7 @@ export class RightBarCreatorsComponent implements OnInit {
   ngOnInit() {
     const defaultTab = this.backendApi.GetStorage(RightBarCreatorsComponent.RightBarTabKey);
     this.activeTab =
-      defaultTab in RightBarCreatorsComponent.chartMap ? defaultTab : RightBarCreatorsComponent.DIAMONDS.name;
+      defaultTab in RightBarCreatorsComponent.chartMap ? defaultTab : RightBarCreatorsComponent.HASHTAGS.name;
     this.selectTab(true);
   }
 
