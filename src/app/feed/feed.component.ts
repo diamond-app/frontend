55dfb79a79a27008db23b71e7a2aad95f431c4ff
superzordon <zordonclout@gmail.com>
2022-08-01 22:30:49 -0400

    Add change detector ref

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -102,6 +102,7 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked {
   constructor(
     private appData: GlobalVarsService,
     private router: Router,
+    private ref: ChangeDetectorRef,
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
     private backendApi: BackendApiService,
@@ -133,8 +134,10 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked {
         this.activeTab = FeedComponent.TAG_TAB;
         // Request the tag feed (so we have it ready for display if needed)
         this.loadingFirstBatchOfTagFeedPosts = true;
+        this.ref.detectChanges();
         this._loadTagFeedPosts(true);
         this.loadingFirstBatchOfTagFeedPosts = false;
+        this.ref.detectChanges();
       } else {
         if (this.activeTab === FeedComponent.TAG_TAB) {
           this.activeTab = FeedComponent.HOT_TAB;
@@ -326,8 +329,10 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked {
     this.router.navigate(["/" + this.globalVars.RouteNames.BROWSE + "/" + this.globalVars.RouteNames.TAG, this.tag], {
       queryParamsHandling: "merge",
     });
+    this.ref.detectChanges();
     this.loadingFirstBatchOfTagFeedPosts = true;
     this._loadTagFeedPosts(true);
+    this.ref.detectChanges();
     this.loadingFirstBatchOfTagFeedPosts = false;
   }
 
