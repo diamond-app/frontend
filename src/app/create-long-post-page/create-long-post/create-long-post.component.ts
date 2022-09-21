2248f5ec3148ec140e4434c93b410b2c32c22406
superzordon <zordonclout@gmail.com>
2022-09-20 21:40:29 -0400

    Convert and upload base64 images to reduce on-chain storage requirements

--- src/app/create-long-post-page/create-long-post/create-long-post.component.ts
+++ src/app/create-long-post-page/create-long-post/create-long-post.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { BackendApiService } from "src/app/backend-api.service";
 import { GlobalVarsService } from "src/app/global-vars.service";
+import { has } from "lodash";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "create-long-post",
@@ -17,7 +19,37 @@ export class CreateLongPostComponent {
     this.content = $event.content;
   }
 
-  publish() {
+  dataURLtoFile(dataurl: string, filename: string): File {
+    let arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  }
+
+  // Loop through all ops in the Delta, convert any images from base64 to a File object, upload them, and then replace
+  // that image in the Delta object with the link to the uploaded image.
+  // This is done to drastically reduce on-chain file size.
+  async uploadAndReplaceBase64Images() {
+    await Promise.all(
+      this.content.ops.map(async (op) => {
+        if (has(op, "insert.image")) {
+          const newFile = this.dataURLtoFile(op.insert.image, "uploaded_image");
+          const res = await this.backendApi
+            .UploadImage(environment.uploadImageHostname, this.appData.loggedInUser.PublicKeyBase58Check, newFile)
+            .toPromise();
+          op.insert.image = res.ImageURL;
+        }
+      })
+    );
+  }
+
+  async publish() {
+    await this.uploadAndReplaceBase64Images();
     this.backendApi
       .SubmitPost(
         this.appData.localNode,
