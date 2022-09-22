bf4803843c90e2b1edd970bbebd7ef76d9cda934
Jackson Dean <jckson.dean@gmail.com>
2022-09-21 20:32:55 -0700

    add recent articles section to blog detail page

--- src/app/blog-page/blog-detail/blog-detail.component.ts
+++ src/app/blog-page/blog-detail/blog-detail.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { BackendApiService, PostEntryResponse } from "src/app/backend-api.service";
+import { BackendApiService, PostEntryResponse, ProfileEntryResponse } from "src/app/backend-api.service";
 import { BlogPostExtraData } from "src/app/create-long-post-page/create-long-post/create-long-post.component";
 import { GlobalVarsService } from "src/app/global-vars.service";
 
@@ -9,7 +9,7 @@ import { GlobalVarsService } from "src/app/global-vars.service";
   templateUrl: "./blog-detail.component.html",
   styleUrls: ["./blog-detail.component.scss"],
 })
-export class BlogDetailComponent implements AfterViewInit {
+export class BlogDetailComponent {
   isLoading = true;
   post: PostEntryResponse;
   recentPosts: PostEntryResponse[] = [];
@@ -18,52 +18,62 @@ export class BlogDetailComponent implements AfterViewInit {
     private backendApi: BackendApiService,
     private globalVars: GlobalVarsService,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    this.route.params.subscribe(() => {
+      // TODO: error handling
+      this.isLoading = true;
+      this.backendApi
+        .GetSinglePost(
+          this.globalVars.localNode,
+          route.snapshot.params.postHashHex /*PostHashHex*/,
+          this.globalVars.loggedInUser?.PublicKeyBase58Check ?? "" /*ReaderPublicKeyBase58Check*/,
+          false /*FetchParents */,
+          0 /*CommentOffset*/,
+          20 /*CommentLimit*/,
+          this.globalVars.showAdminTools() /*AddGlobalFeedBool*/,
+          2 /*ThreadLevelLimit*/,
+          1 /*ThreadLeafLimit*/,
+          false /*LoadAuthorThread*/
+        )
+        .toPromise()
+        .then(({ PostFound }) => {
+          this.post = PostFound;
+          return PostFound;
+        })
+        .then((post: PostEntryResponse) => {
+          // NOTE: don't return this since it would block rendering for the
+          // initial post fetch.
+          this._fetchRecentPosts(post.ProfileEntryResponse);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    });
+  }
 
-  ngAfterViewInit(): void {
-    // TODO: error handling
+  _fetchRecentPosts(profile: ProfileEntryResponse) {
     this.backendApi
-      .GetSinglePost(
+      .GetPostsForPublicKey(
         this.globalVars.localNode,
-        this.route.snapshot.params.postHashHex /*PostHashHex*/,
-        this.globalVars.loggedInUser?.PublicKeyBase58Check ?? "" /*ReaderPublicKeyBase58Check*/,
-        false /*FetchParents */,
-        0 /*CommentOffset*/,
-        20 /*CommentLimit*/,
-        this.globalVars.showAdminTools() /*AddGlobalFeedBool*/,
-        2 /*ThreadLevelLimit*/,
-        1 /*ThreadLeafLimit*/,
-        false /*LoadAuthorThread*/
+        "",
+        profile.Username,
+        this.globalVars.loggedInUser?.PublicKeyBase58Check,
+        "",
+        1000, // NOTE: we currently cant filter to only blog posts on the api, just get the last 1000 posts and filter them.
+        false /*MediaRequired*/
       )
       .toPromise()
-      .then(({ PostFound }) => {
-        this.post = PostFound;
-        this.backendApi
-          .GetPostsForPublicKey(
-            this.globalVars.localNode,
-            "",
-            PostFound.PostEntryResponse.Username,
-            this.globalVars.loggedInUser?.PublicKeyBase58Check,
-            "",
-            1000, // NOTE: we currently cant filter to only blog posts on the api, just get the last 1000 posts and filter them.
-            false /*MediaRequired*/
-          )
-          .toPromise()
-          .then(({ Posts }) =>
-            // Filter to only posts that have a blog post rich text extra data field.
-            Posts.filter(
-              (p: PostEntryResponse) => typeof (p.PostExtraData as BlogPostExtraData).BlogDeltaRtfFormat !== "undefined"
-            ).slice(10)
-          )
-          .then((posts) => {
-            this.recentPosts = posts.map((p: PostEntryResponse) => ({
-              ...p,
-              ProfileEntryResponse: PostFound.PostEntryResponse,
-            }));
-          });
-      })
-      .finally(() => {
-        this.isLoading = false;
+      .then(({ Posts }) =>
+        // Filter to only posts that have a blog post rich text extra data field.
+        Posts.filter(
+          (p: PostEntryResponse) => typeof (p.PostExtraData as BlogPostExtraData).BlogDeltaRtfFormat !== "undefined"
+        ).slice(0, 10)
+      )
+      .then((posts) => {
+        this.recentPosts = posts.map((p: PostEntryResponse) => ({
+          ...p,
+          ProfileEntryResponse: profile,
+        }));
       });
   }
 }
