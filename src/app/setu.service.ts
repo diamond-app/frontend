44d01692897815e5c6956235f89b516e49b0de39
Jackson Dean <jckson.dean@gmail.com>
2022-11-17 17:58:39 -0800

    basic setu service

--- src/app/setu.service.ts
+++ src/app/setu.service.ts
@@ -1,19 +1,131 @@
+//@ts-strict
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map, switchMap } from "rxjs/operators";
+import { GlobalVarsService } from "src/app/global-vars.service";
+import { IdentityService } from "src/app/identity.service";
 import { environment } from "src/environments/environment";
 
 const buildURL = (endpoint: string) => `${environment.setuAPI}/${endpoint}`;
+
+type SubscriptionType = "all_tweets" | "include_hashtags" | "exclude_hashtags";
+
+interface SubscriptionParams {
+  public_key: string; // deso public key
+  twitter_user_id: string;
+  subscription_type: SubscriptionType;
+  hashtags: string; // comma delimited string: "#web3,#deso,#setu"
+  include_retweets: boolean;
+  include_quote_tweets: boolean;
+}
+
+export type GetCurrentSubscriptionsResponse = {
+  status: string;
+} & Omit<SubscriptionParams, "public_key">;
+
+export interface GetDerivedKeyStatusResponse {
+  is_expired: false;
+  status: string;
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class SetuService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private identity: IdentityService, private globalVars: GlobalVarsService) {}
 
-  getSettings() {
-    return this.http.post(buildURL("real-time-sync/get-current-subscriptions"), {
-      public_key: "BC1YLivYU6g9w3LXNnS7Amiji3AoQQjDNKgTX8GEeaTo7J9551nFCTB",
-      twitter_user_id: "1470865227570814976",
+  getDerivedKeyStatus(parentPublicKey: string): Observable<GetDerivedKeyStatusResponse> {
+    return this.http.post<GetDerivedKeyStatusResponse>(buildURL("real-time-sync/get-derived-key-status"), {
+      public_key: parentPublicKey,
     });
+  }
+
+  updateDerivedKey(params: {
+    publicKey: string;
+    derivedSeedHex: string;
+    derivedPublicKey: string;
+    btcDepositAddress: string;
+    ethDepositAddress: string;
+    expirationBlock: number;
+    network: string;
+    accessSignature: string;
+  }): Observable<{ status: string }> {
+    return this.getJwt().pipe(
+      switchMap((jwt) => {
+        return this.http.post<{ status: string }>(buildURL("real-time-sync/derived-cred-callback"), {
+          ...params,
+          jwt,
+        });
+      })
+    );
+  }
+
+  // Q: when would we ever set this to anything other than 1?
+  changeSignedStatus(params: { public_key: string; is_signed: 1 | 0 }): Observable<{ status: string }> {
+    return this.getJwt().pipe(
+      switchMap((jwt) => {
+        return this.http.post<{ status: string }>(buildURL("real-time-sync/change-sign-status"), {
+          ...params,
+          jwt,
+        });
+      })
+    );
+  }
+
+  getCurrentSubscription(
+    params: Pick<SubscriptionParams, "public_key" | "twitter_user_id">
+  ): Observable<GetCurrentSubscriptionsResponse> {
+    return this.http.post<GetCurrentSubscriptionsResponse>(
+      buildURL("real-time-sync/get-current-subscriptions"),
+      params
+    );
+  }
+
+  createSubscription(
+    params: SubscriptionParams & { username_deso: string; twitter_username: string }
+  ): Observable<GetCurrentSubscriptionsResponse> {
+    return this.getJwt().pipe(
+      switchMap((jwt) => {
+        return this.http.post<GetCurrentSubscriptionsResponse>(buildURL("real-time-sync/create-subscription"), {
+          ...params,
+          jwt,
+        });
+      })
+    );
+  }
+
+  changeSubscription(params: SubscriptionParams): Observable<GetCurrentSubscriptionsResponse> {
+    return this.getJwt().pipe(
+      switchMap((jwt) => {
+        return this.http.post<GetCurrentSubscriptionsResponse>(buildURL("real-time-sync/change-subscription-type"), {
+          ...params,
+          jwt,
+        });
+      })
+    );
+  }
+
+  unsubscribe(params: Pick<SubscriptionParams, "public_key" | "twitter_user_id">): Observable<{ status: string }> {
+    return this.getJwt().pipe(
+      switchMap((jwt) => {
+        return this.http.post<{ status: string }>(buildURL("real-time-sync/unsubscribe"), {
+          ...params,
+          jwt,
+        });
+      })
+    );
+  }
+
+  private getJwt(): Observable<string> {
+    if (!this.globalVars.loggedInUser) {
+      throw new Error("Cannot get jwt without a logged in user");
+    }
+
+    return this.identity
+      .jwt({
+        ...this.identity.identityServiceParamsForKey(this.globalVars.loggedInUser.PublicKeyBase58Check),
+      })
+      .pipe(map(({ jwt }) => jwt));
   }
 }
--- src/app/twitter-sync-settings/twitter-sync-settings.component.ts
+++ src/app/twitter-sync-settings/twitter-sync-settings.component.ts
@@ -1,7 +1,17 @@
 //@ts-strict
 import { Component, OnDestroy } from "@angular/core";
+import { forkJoin } from "rxjs";
 import { first } from "rxjs/operators";
-import { SetuService } from "src/app/setu.service";
+import { GlobalVarsService } from "src/app/global-vars.service";
+import { GetCurrentSubscriptionsResponse, GetDerivedKeyStatusResponse, SetuService } from "src/app/setu.service";
+
+interface TwitterUserData {
+  twitter_user_id: string;
+  twitter_user_profile_image: string;
+  twitter_username: string;
+}
+
+const TWITTER_USER_DATA_LOCAL_STORAGE_KEY = "twitterUserData";
 
 @Component({
   selector: "app-twitter-sync-settings",
@@ -9,25 +19,20 @@ import { SetuService } from "src/app/setu.service";
   styleUrls: ["./twitter-sync-settings.component.scss"],
 })
 export class TwitterSyncSettingsComponent implements OnDestroy {
-  settings: any;
+  isDestroyed: boolean = false;
+  twitterUserData?: TwitterUserData;
+  boundPostMessageListener?: (event: MessageEvent) => void;
+  setuSubscriptions?: GetCurrentSubscriptionsResponse;
+  derivedKeyStatus?: GetDerivedKeyStatusResponse;
 
-  private messageListener = (event: MessageEvent) => {
-    console.log(event);
-    if (event.origin !== "https://web3setu.com") return;
-    console.log("setu event!", event);
-  };
-
-  constructor(private setu: SetuService) {
-    window.addEventListener("message", this.messageListener);
-    this.setu
-      .getSettings()
-      .pipe(first())
-      .subscribe((res) => {
-        console.log(res);
-      });
+  constructor(private setu: SetuService, private globalVars: GlobalVarsService) {
+    this.updateTwitterUserData();
   }
 
   loginWithTwitter() {
+    this.boundPostMessageListener = this.postMessageListener.bind(this);
+    window.addEventListener("message", this.boundPostMessageListener);
+
     const h = 1000;
     const w = 800;
     const y = window.outerHeight / 2 + window.screenY - h / 2;
@@ -41,6 +46,76 @@ export class TwitterSyncSettingsComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
-    window.removeEventListener("message", this.messageListener);
+    this.isDestroyed = true;
+    if (this.boundPostMessageListener) {
+      window.removeEventListener("message", this.boundPostMessageListener);
+    }
+  }
+
+  private updateTwitterUserData() {
+    if (this.globalVars.loggedInUser) {
+      this.twitterUserData = this.getStoredTwitterUser();
+      if (this.twitterUserData) {
+        forkJoin([
+          this.setu.getCurrentSubscription({
+            public_key: this.globalVars.loggedInUser.PublicKeyBase58Check,
+            twitter_user_id: this.twitterUserData.twitter_user_id,
+          }),
+          this.setu.getDerivedKeyStatus(this.globalVars.loggedInUser.PublicKeyBase58Check),
+        ])
+          .pipe(first())
+          .subscribe(
+            ([subscription, derivedKeyStatus]) => {
+              this.setuSubscriptions = subscription;
+              this.derivedKeyStatus = derivedKeyStatus;
+            },
+            (err) => {
+              // TODO: error handling
+            }
+          );
+      }
+    }
+  }
+
+  private postMessageListener(event: MessageEvent) {
+    if (event.origin !== "https://web3setu.com") return;
+    if (!event.data.twitter_user_id) {
+      throw new Error("twitter user data missing");
+    }
+
+    this.setStoredTwitterUser(event.data);
+    this.twitterUserData = event.data as TwitterUserData;
+    this.updateTwitterUserData();
+  }
+
+  private getStoredTwitterUser(): TwitterUserData | undefined {
+    if (!this.globalVars.loggedInUser) {
+      throw new Error("Cannot get twitter data without a logged in user.");
+    }
+
+    const storedTwitterUserData = window.localStorage.getItem(
+      `twitterUserData_${this.globalVars.loggedInUser.PublicKeyBase58Check}`
+    );
+
+    if (storedTwitterUserData) {
+      return JSON.parse(storedTwitterUserData);
+    }
+
+    return undefined;
+  }
+
+  private setStoredTwitterUser(data: TwitterUserData) {
+    if (!this.globalVars.loggedInUser) {
+      throw new Error("Cannot set twitter data without a logged in user.");
+    }
+
+    // NOTE: we key this on the logged in user's public key in case the user
+    // links their twitter account with one user, then switches accounts. We
+    // also ensure that only 1 deso user can be linked to a given twitter
+    // account.
+    window.localStorage.setItem(
+      `${TWITTER_USER_DATA_LOCAL_STORAGE_KEY}_${this.globalVars.loggedInUser.PublicKeyBase58Check}`,
+      JSON.stringify(data)
+    );
   }
 }
