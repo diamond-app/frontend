ff804e43c5676e79fd6a747ea19a48e916b75eab
Jackson Dean <jckson.dean@gmail.com>
2022-12-06 11:21:48 -0800

    add spacing at the bottom for mobile

--- src/app/twitter-sync-page/twitter-sync-page.component.ts
+++ src/app/twitter-sync-page/twitter-sync-page.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { GlobalVarsService } from "src/app/global-vars.service";
 
 @Component({
   selector: "app-twitter-sync-page",
   templateUrl: "./twitter-sync-page.component.html",
   styleUrls: ["./twitter-sync-page.component.scss"],
 })
-export class TwitterSyncPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TwitterSyncPageComponent {
+  constructor(public globalVars: GlobalVarsService) {}
 }
