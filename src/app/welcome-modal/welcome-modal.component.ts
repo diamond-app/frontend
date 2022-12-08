37b176e26f6339effbcc01286633e456187139d9
Jackson Dean <jckson.dean@gmail.com>
2022-12-08 14:46:57 -0800

    WIP

--- src/app/welcome-modal/welcome-modal.component.ts
+++ src/app/welcome-modal/welcome-modal.component.ts
@@ -1,6 +1,7 @@
 //@ts-strict
 import { Component } from "@angular/core";
 import { BsModalRef } from "ngx-bootstrap/modal";
+import { GlobalVarsService } from "src/app/global-vars.service";
 
 @Component({
   selector: "app-welcome-modal",
@@ -8,5 +9,9 @@ import { BsModalRef } from "ngx-bootstrap/modal";
   styleUrls: ["./welcome-modal.component.scss"],
 })
 export class WelcomeModalComponent {
-  constructor(public bsModalRef: BsModalRef) {}
+  constructor(public bsModalRef: BsModalRef, private globalVars: GlobalVarsService) {}
+
+  login() {
+    alert("login");
+  }
 }
