53fd9f9f39fe32eeabf9534d29018bd7c3116834
superzordon <zordonclout@gmail.com>
2022-12-13 13:18:03 -0500

    Update function call

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -82,7 +82,6 @@ export class SettingsComponent implements OnInit {
       if (queryParams?.publicKey) {
         this.userPublicKeyBase58Check = queryParams.publicKey;
       }
-      this.initializeAppUser();
     });
   }
 
@@ -100,7 +99,7 @@ export class SettingsComponent implements OnInit {
           throw err;
         })
       );
-      if (!!loggedInUser?.ProfileEntryResponse) {
+      if (!!loggedInUser?.ProfileEntryResponse || true) {
         const getUserMetadataObs = this.backendApi.GetUserGlobalMetadata(this.globalVars.localNode, userPublicKey);
         forkJoin([getAppUserObs, getUserMetadataObs])
           .pipe(
@@ -142,6 +141,7 @@ export class SettingsComponent implements OnInit {
   ngOnInit() {
     this.titleService.setTitle(`Settings - ${environment.node.name}`);
     this.selectedLanguage = this.translocoService.getActiveLang();
+    this.initializeAppUser();
   }
 
   closeModal() {
