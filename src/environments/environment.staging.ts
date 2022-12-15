37bf4038da1dc7d139972137f7dc0a6cf70eeaea
superzordon <zordonclout@gmail.com>
2022-12-15 16:10:05 -0500

    Add staging env

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,28 @@
+export const environment = {
+  production: true,
+  uploadImageHostname: "node.deso.org",
+  verificationEndpointHostname: "https://node.deso.org",
+  apiInternalHostname: "https://diamondapp.com",
+  uploadVideoHostname: "node.deso.org",
+  identityURL: "https://identity.deso.org",
+  supportEmail: "help@diamondapp.com",
+  dd: {
+    apiKey: "DCEB26AC8BF47F1D7B4D87440EDCA6",
+    jsPath: "https://diamondapp.com/tags.js",
+    ajaxListenerPath: "diamondapp.com/api",
+    endpoint: "https://diamondapp.com/js/",
+  },
+  amplitude: {
+    key: "26d4aa5c3210ab69cfb76b7951030db5",
+    domain: "amp.diamondapp.com",
+  },
+  node: {
+    id: 3,
+    name: "Diamond",
+    url: "https://diamondapp.com",
+    logoAssetDir: "/assets/diamond/",
+  },
+  megaswapURL: "https://megaswap.xyz",
+  megaswapAPI: "https://megaswap.dev",
+  setuAPI: "https://web3setu.co.in",
+};
