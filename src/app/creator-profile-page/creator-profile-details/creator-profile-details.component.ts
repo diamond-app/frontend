5ee9fe65c03d6dfb4c94c932007ffdb7bcdffe15
superzordon <zordonclout@gmail.com>
2023-02-16 14:11:01 -0500

    Set meta tags for profile

--- src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
+++ src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
@@ -207,6 +207,7 @@ export class CreatorProfileDetailsComponent implements OnInit {
           return;
         }
         this.profile = res.Profile;
+        this.setMetaTags();
         this.loading = false;
       },
       (_) => {
