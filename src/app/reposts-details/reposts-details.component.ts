60e90463b8108874a26139848265974220241ecd
stas-kh <stas@deso.org>
2023-04-18 12:15:07 +0300

    fix: adapting reposts

--- src/app/reposts-details/reposts-details.component.ts
+++ src/app/reposts-details/reposts-details.component.ts
@@ -15,7 +15,6 @@ export class RepostsDetailsComponent implements OnInit {
   @Input() postHashHex: string;
   @Input() bsModalRef;
   diamonds = [];
-  loading = false;
   errorLoading = false;
 
   constructor(
@@ -26,7 +25,6 @@ export class RepostsDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loading = true;
     if (!this.postHashHex) {
       this.postHashHex = this.route.snapshot.params.postHashHex;
     }
@@ -42,7 +40,6 @@ export class RepostsDetailsComponent implements OnInit {
     if (!isNil(this.lastPage) && page > this.lastPage) {
       return [];
     }
-    this.loading = true;
 
     return this.backendApi
       .GetRepostsForPost(
@@ -63,8 +60,6 @@ export class RepostsDetailsComponent implements OnInit {
           if (repostersPage.length < this.pageSize) {
             this.lastPage = page;
           }
-
-          this.loading = false;
 
           // Return the page.
           return repostersPage;
